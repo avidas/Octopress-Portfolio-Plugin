098b09c829d4a713e3060c82a83a38f6fad9fc0b
Rene <devproren@gmail.com>
2014-05-25 23:49:44 -0700

    refactored code to generate html for portfolio index page

--- portfolio.rb
+++ portfolio.rb
@@ -4,6 +4,7 @@
 #
 
 module Jekyll
+
   class Portfolio < Liquid::Tag
 
     def initialize(tag_name, id, tokens)
@@ -11,68 +12,62 @@ module Jekyll
       @project_folder = id.to_s.strip
     end
 
+    def gen_project_list(portfolio_dir_path, portfolio_root)
+
+      portfolio_list_html = ""
+      projects = Dir.glob(portfolio_dir_path + '/*').select {|f| File.directory? f}
+      projects.each do |project_path|
+        index_file_path = 'index.markdown'
+
+        Dir.chdir(project_path)
+
+        if File.exists? index_file_path
+          link = portfolio_root + "/" + File.basename(project_path)
+          img_files = Dir.glob('main-image.*')
+
+          project_yaml = YAML::load(File.read(index_file_path))
+
+          brief_desc = project_yaml['brief']
+          proj_title = project_yaml['title'] #assume every file has a title in YAML front matter
+
+          # generate portfolio item html
+          portfolio_list_html += '<div class="portfolio-item cf ">' +
+            '<div class="portfolio-info ">' +
+            '<a href="' + link + '">' +
+            '<h3>'+ proj_title + '</h3>' +
+            '</a>'
+          if not brief_desc.nil?
+            portfolio_list_html += '<p>'+ brief_desc + '</p>'
+          end
+
+          portfolio_list_html += '</div>'
+
+          if img_files.size == 1 # each project folder should have only one main-image
+            alt = proj_title + ' main image'
+            img = link + '/' + img_files[0]
+            portfolio_list_html += '<a title=main-image href="' + link + '">' +
+              '<img alt="' + alt + '" src="' + img + '">' +
+              '</a>'
+          end
+
+          portfolio_list_html += '</div>'
+
+        end
+      end
+      return portfolio_list_html
+    end
+
     def render(context)
-
       content = ""
-      projects = []
 
-      portfolio_root = context.registers[:site].config['portfolio_root']             # /portfolio
-      portfolio_dir_path = context.registers[:site].config['portfolio_path']         # /Volumes/Macintosh HD/Sebastian/Sites/octopress/source/portfolio
+      portfolio_root = context.registers[:site].config['portfolio_root']
+      portfolio_dir_path = context.registers[:site].config['portfolio_path']
 
-      portfolio_dir = Dir.new portfolio_dir_path
 
       if(@project_folder == "")
-
-        portfolio_dir.each do |project|
-          if(project != "." && project != ".." && project != ".DS_Store" && project != "index.markdown")   # Added .DS_Store and index.markdown
-            projects.push(project)
-
-          end
-        end
-
-        projects.each do |project_name|
-          Dir.foreach(portfolio_dir_path + "/" + project_name) do |filename|
-            if(filename != "." && filename != ".." && filename != ".DS_Store")
-
-              link = portfolio_root + "/" + project_name
-              img = link + "/" + filename   #changed.
-
-              title = File.basename(filename, File.extname(filename))
-
-              if(filename.index "main")
-                content += '<div class="portfolio-item cf ">'
-                project_dir_path = portfolio_dir_path + "/" + project_name
-                index_files = Dir.glob(project_dir_path + "/" + "index.*")
-                if not index_files.empty?
-                    # load the first index file found in directory, presumably in .markdown, although can be in other formats that is YAML compatible.
-
-                    project_index_file = File.read(index_files[0])
-                    project_yaml = YAML::load(project_index_file)
-                    brief_desc = project_yaml['brief']
-                    title_name = project_yaml['title'] #assume every file has a title in YAML front matter
-
-                    if not brief_desc.nil?
-                        content +=
-                                    '<div class="portfolio-info ">' +
-                                        '<a href="' + link + '">' +
-                                            '<h3>'+ title_name + '</h3>' +
-                                        '</a>' +
-                                        '<p>'+ brief_desc + '</p>' +
-                                    '</div>' +
-                                    '<a title="' + title + '" href="' + link + '">' +
-                                        '<img src="' + img + '">' +
-                                    '</a>'
-                    end
-                else
-                    content += '<a title="' + title + '" href="' + link + '"><h3>'+ title_name + '</h3><img src="' + img + '"></a>'
-                end
-                content += '</div>'
-              end
-            end
-          end
-        end
+        content += gen_project_list(portfolio_dir_path, portfolio_root)
       else
-
+        # generate pics for the project page
         Dir.foreach(portfolio_dir_path + "/" + @project_folder) do |filename|
           if(filename != "." && filename != "..")
 
@@ -81,17 +76,17 @@ module Jekyll
             title = File.basename(filename, File.extname(filename))
 
             real_title = title.sub("small-", "")
-            big_img = img.sub("small-", "")
 
             if(title.index "small-")
-              content += '<a title="' + real_title + '" href="' + big_img + '" class="fancybox"><img src="' + img + '"></a>'
+              content += '<a title="' + real_title + '" href="' + img + '"><img src="' + img + '"></a>'
             end
             if(title.index "main-")
-              content += '<a title="' + real_title + '" href="' + big_img + '" class="fancybox"><img src="' + img + '"></a>' # remove if you don't need main-* image on the portfolio page.
+              content += '<a title="' + real_title + '" href="' + img + '"><img src="' + img + '"></a>' # remove if you don't need main-* image on the portfolio page.
             end
           end
         end
       end
+
       return content
     end
   end
